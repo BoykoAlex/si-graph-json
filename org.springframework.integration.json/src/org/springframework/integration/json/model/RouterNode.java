6a83d74d12bf7caedb60083ec66dd69180b7f519
BoykoAlex <aboyko@pivotal.io>
2014-11-28 17:33:30 -0500

    Router hack to handle channels in expressions attribute

--- org.springframework.integration.json/src/org/springframework/integration/json/model/RouterNode.java
+++ org.springframework.integration.json/src/org/springframework/integration/json/model/RouterNode.java
@@ -2,6 +2,8 @@ package org.springframework.integration.json.model;
 
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 import org.springframework.integration.json.IntegrationSchemaConstants;
 import org.w3c.dom.Element;
@@ -35,8 +37,54 @@ public class RouterNode extends ThroughNode {
 			props.put(IntegrationSchemaConstants.ATTR_RESOLUTION_REQUIRED, null);
 			props.put(IntegrationSchemaConstants.ATTR_IGNORE_CHANNEL_NAME_RESOLUTION_FAILURES, null);
 			props.put(IntegrationSchemaConstants.ATTR_TIMEOUT, null);
+			props.put(IntegrationSchemaConstants.ATTR_EXPRESSION, null);
 		}
 		return props;
+	}
+
+	@Override
+	public List<Link> getSourceLinksFromXml(
+			Map<String, IntModelElement> referenceRegistry) {
+		List<Link> sourceLinksFromXml = super.getSourceLinksFromXml(referenceRegistry);
+		if (element.hasAttribute(IntegrationSchemaConstants.ATTR_INPUT_CHANNEL) && getProperties() != null) {
+			String expression = (String) getProperties().get(IntegrationSchemaConstants.ATTR_EXPRESSION);
+			if (expression != null) {
+				for (String id : Pattern.compile("\\W").split(expression)) {
+					IntModelElement target = referenceRegistry.get(id);
+					if (target instanceof IEnumeratedModelElement) {
+						Link link = new Link(Constants.TRANSITION_TYPE_SOLID, getId(),
+								((IEnumeratedModelElement)target).getId());
+						sourceLinksFromXml.add(link);
+					}
+				}
+			}
+		}
+		return sourceLinksFromXml;
+	}
+	
+	@Override
+	public List<Link> getTargetLinksFromXml(
+			Map<String, IntModelElement> referenceRegistry) {
+		List<Link> sourceLinksFromXml = super
+				.getSourceLinksFromXml(referenceRegistry);
+		if (element
+				.hasAttribute(IntegrationSchemaConstants.ATTR_OUTPUT_CHANNEL)
+				&& getProperties() != null) {
+			String expression = (String) getProperties().get(
+					IntegrationSchemaConstants.ATTR_EXPRESSION);
+			if (expression != null) {
+				for (String id : Pattern.compile("\\W").split(expression)) {
+					IntModelElement source = referenceRegistry.get(id);
+					if (source instanceof IEnumeratedModelElement) {
+						Link link = new Link(Constants.TRANSITION_TYPE_SOLID,
+								((IEnumeratedModelElement) source).getId(),
+								getId());
+						sourceLinksFromXml.add(link);
+					}
+				}
+			}
+		}
+		return sourceLinksFromXml;
 	}	
 
 }
